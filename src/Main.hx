820bc60c0bcabff2c28dd0ce8c4751de94306ee6
Paweł Miczka <p.miczka@statscore.com>
2021-04-15 13:05:51 +0200

    cleaned Main file

--- src/Main.hx
+++ src/Main.hx
@@ -1,12 +1,3 @@
-using lua.TableTools;
-
-enum abstract VehicleModel(Int) {
-	var SAVANNA = 567;
-	var OTHER_STUFF = 1234;
-}
-
-typedef User = {name:String, email:String};
-
 class Main {
 	static function main() {}
 }
